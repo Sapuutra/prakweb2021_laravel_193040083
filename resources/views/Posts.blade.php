02a78453997cc91d225fb9fdb1f9de778213b1c4
Sapuutra <gigihdsaputra@gmail.com>
2021-11-12 22:59:30 +0700

    struktur folder,routes and view

---
+++ resources/views/Posts.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>WPU Blog | {{ $tittle }}</title>
+  </head>
+
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div class="container">
+    <a class="navbar-brand" href="#">WPU Blog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/blog">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<body>
+<div class="container mt-4">
+        <h1>Halaman Post</h1>
+    </div>
+    <body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia hic repellendus beatae, eos voluptatum ab, animi dicta magnam harum nihil! Atque illum dolor nam repellat rem praesentium, nisi quis cum magni a corrupti. Ipsum vel vero quod enim reprehenderit molestias ullam quibusdam sit eligendi veniam saepe pariatur labore odio assumenda excepturi recusandae dolores, fugit hic laudantium exercitationem nostrum quo. Sit veritatis obcaecati totam, eos repudiandae inventore dignissimos aperiam atque modi perferendis et illo iste, quas repellendus quaerat architecto nobis. Ex autem quasi nisi repudiandae fugit totam aut recusandae non eligendi ab reprehenderit, rerum minima! Quod cum obcaecati nostrum distinctio!
+</body>
+
+</body>
+</html>
